ddd1f76ef45826ea3c1544a843881620e1ee2a22
Liz <lizriderwilson@gmail.com>
2021-01-09 12:38:55 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-# require "pry"
+
 
 class Song
   attr_accessor :name, :artist_name
@@ -13,58 +13,51 @@ class Song
   end
 
   def self.create
-    new_song = self.new
-    @@all << new_song
-    new_song
+    song = self.new
+    song.save
+    song
   end
 
-  def self.new_by_name(song_name)
-    new_song = self.new
-    new_song.name = song_name
-    new_song
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
   end
 
-  def self.create_by_name(song_name)
-    new_song = self.new
-    new_song.name = song_name
-    @@all << new_song
-    new_song
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
   end
 
-  def self.find_by_name(song_name)
-    @@all.find{|song| song.name == song_name}
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
   end
 
-  def self.find_or_create_by_name(song_name)
-    if self.find_by_name(song_name) == nil
-      self.create_by_name(song_name)
-    else
-      self.find_by_name(song_name)
-    end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
 
   def self.alphabetical
-    @@all.sort_by! {|song| song.name}
+    self.all.sort_by {|song| song.name}
   end
 
-  def self.new_from_filename(song_file)
-    song_file.slice!(/[.]\w+/)
-    attributes = song_file.split(" - ")
-    split_artist = attributes[0]
-    split_song = attributes[1]
-    # binding.pry
-    new_song = self.new
-    new_song.name = split_song
-    new_song.artist_name = split_artist
+  def self.new_from_filename(filename)
+    artist_name, song_name = filename.gsub(/[.]\w+/, '').split(" - ")
+    new_song = self.new_by_name(song_name)
+    new_song.artist_name = artist_name
     new_song
   end
 
-  def self.create_from_filename(song_name)
-    @@all << self.new_from_filename(song_name)
+  def self.create_from_filename(filename)
+    artist_name, song_name = filename.gsub(/[.]\w+/, '').split(" - ")
+    new_song = self.create_by_name(song_name)
+    new_song.artist_name = artist_name
+    new_song
   end
 
   def self.destroy_all
-    @@all.clear
+    self.all.clear
   end
-  
+
 end
